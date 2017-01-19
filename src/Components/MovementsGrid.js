3ea9d8b1537a980ad53dc7bb8020e098e35fc3a4
Pablo Núñez <pablonete@gmail.com>
2017-01-19 01:03:51 +0100

    Playing with input.

--- src/Components/MovementsGrid.js
+++ src/Components/MovementsGrid.js
@@ -1,11 +1,63 @@
 import Inferno from 'inferno';
+import Component from 'inferno-component';
 
-export const MovementsGrid = (props) =>
-  <div>
-    {[1, 2, 3, 4, 5].map(i => <MovementRow index={i} />)}
-  </div>;
+export class MovementsGrid extends Component {
+  state = { movements: [] };
+
+  render() {
+    return (
+      <div>
+        <MovementEditRow onSaved={this.createMovement} />
+        {this.state.movements.map(m => <MovementRow movement={m} />)}
+      </div>);
+  }
+
+  createMovement = (movement) => {
+    this.setState({
+      movements: [
+        ...this.state.movements,
+        movement,
+      ],
+    });
+  }
+}
 
 const MovementRow = (props) =>
   <div>
-    {props.index}
+    {props.movement.date}
+    -
+    {props.movement.title}
+    -
+    {props.movement.amount}
   </div>;
+
+class MovementEditRow extends Component {
+  render() {
+    return (
+      <div>
+        <input placeholder="Date" onChange={this.onDateChanged} />
+        <input placeholder="Title" onChange={this.onTitleChanged} />
+        <input placeholder="Amount" onChange={this.onAmountChanged} />
+        <button onClick={this.onSaved}>Save</button>
+      </div>);
+  }
+
+  onDateChanged = (event) =>
+    this.setState({ date: event.target.value });
+
+  onTitleChanged = (event) =>
+    this.setState({ title: event.target.value });
+
+  onAmountChanged = (event) =>
+    this.setState({ amount: event.target.value });
+
+  onSaved = () => {
+    if (this.props.onSaved) {
+      this.props.onSaved({
+        date: this.state.date,
+        title: this.state.title,
+        amount: this.state.amount,
+      });
+    }
+  }
+}
